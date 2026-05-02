aef0afcf1a36116a84bff2c7cef286390e05f992
yash70g <yash70g@gmail.com>
2026-05-02 13:02:16 +0530

    complex seperation

--- tokenizer-visualizer/tokenizer.js
+++ tokenizer-visualizer/tokenizer.js
@@ -1,10 +1,27 @@
-// Simple tokenizer: splits text into words and punctuation
+
 function tokenize(text) {
-  // This regex splits on word boundaries and keeps punctuation as separate tokens
-  return text.match(/\w+|[^\w\s]/g) || [];
+  text = text.replace(/\r?\n|\r/g, ' ');
+  let roughTokens = text.split(/\s+/).filter(Boolean);
+  let tokens = [];
+  for (let token of roughTokens) {
+    //  contractions  don't -> don 't
+    token = token.replace(/(\w+)'(\w+)/g, "$1 '$2");
+    // Split on punctuation
+    let subTokens = token.split(/([!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~])/).filter(Boolean);
+    for (let sub of subTokens) {
+      // Further split long words into subwords 
+      if (sub.length > 6 && /^[a-zA-Z]+$/.test(sub)) {
+        let mid = Math.floor(sub.length / 2);
+        tokens.push(sub.slice(0, mid));
+        tokens.push(sub.slice(mid));
+      } else {
+        tokens.push(sub);
+      }
+    }
+  }
+  return tokens;
 }
 
-// Export for use in browser
 if (typeof window !== 'undefined') {
   window.tokenize = tokenize;
 }
