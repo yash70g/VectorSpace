0f36e5a3432026fce2d1fac6621b70379eb2fa2d
yash70g <yash70g@gmail.com>
2026-05-02 13:22:11 +0530

    strict match

--- tokenizer-visualizer/vocab.js
+++ tokenizer-visualizer/vocab.js
@@ -6,13 +6,29 @@ function buildVocabulary(tokens) {
   // array of [token, count]
   return Object.entries(vocab).sort((a, b) => b[1] - a[1]);
 }
+function hashString(str) {
+  let hash = 5381;
+  for (let i = 0; i < str.length; i++) {
+    hash = ((hash << 5) + hash) + str.charCodeAt(i);
+  }
+  return hash >>> 0;
+}
+function mulberry32(seed) {
+  return function() {
+    let t = seed += 0x6D2B79F5;
+    t = Math.imul(t ^ t >>> 15, t | 1);
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+  }
+}
 
-// Generate random vectors for each token
 function generateRandomVectors(vocab, dim = 8) {
   // vocab: array of [token, count]
   const vectors = {};
   vocab.forEach(([token]) => {
-    vectors[token] = Array.from({ length: dim }, () => +(Math.random() * 2 - 1).toFixed(3));
+    const seed = hashString(token);
+    const rand = mulberry32(seed);
+    vectors[token] = Array.from({ length: dim }, () => +(rand() * 2 - 1).toFixed(3));
   });
   return vectors;
 }
