cf42dfc57fd015f996f3ff25cd751373a01ebf7d
yash70g <yash70g@gmail.com>
2026-05-02 13:05:54 +0530

    common vocab

--- tokenizer-visualizer/vocab.js
+++ tokenizer-visualizer/vocab.js
@@ -1,10 +1,9 @@
-// Build vocabulary from a list of tokens
 function buildVocabulary(tokens) {
   const vocab = {};
   tokens.forEach(token => {
     vocab[token] = (vocab[token] || 0) + 1;
   });
-  // Convert to sorted array of [token, count]
+  // array of [token, count]
   return Object.entries(vocab).sort((a, b) => b[1] - a[1]);
 }
 
